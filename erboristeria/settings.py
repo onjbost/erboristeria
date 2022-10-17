e2d4df6db91df59f794300f595fedec291ecb069
onjbost <gattuccio.mattia@gmail.com>
2022-10-17 16:30:36 +0200

    upgraded sharing

--- erboristeria/settings.py
+++ erboristeria/settings.py
@@ -122,9 +122,7 @@ STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-# STATIC_ROOT = '/home/onjbost/site/erboristeria/static'
-STATIC_ROOT = ''
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'), )
+STATIC_ROOT = '/home/onjbost/site/erboristeria/static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
